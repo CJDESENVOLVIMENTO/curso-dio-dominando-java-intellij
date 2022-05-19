c2c69b0695a3c3bc411bfa46205c35c72a0fd5f4
CJDESENVOLVIMENTO <105030804+CJDESENVOLVIMENTO@users.noreply.github.com>
2022-05-18 22:51:42 -0300

    Criando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,8 +7,45 @@ public class PrimeiroPrograma {
         Dados dados = new Dados();
         System.out.println(dados);
 
+        Livro livro1 = new Livro("Financeiro", 300);
+        System.out.println(livro1);
+        
      /*   int a = 5;
         int b = 3;
         System.out.println("CJindev");*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer nmpagina;
+
+    public Livro(String nome, Integer nmpagina) {
+        this.nome = nome;
+        this.nmpagina = nmpagina;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNmpagina() {
+        return nmpagina;
+    }
+
+    public void setNmpagina(Integer nmpagina) {
+        this.nmpagina = nmpagina;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", nmpagina=" + nmpagina +
+                '}';
     }
 }
